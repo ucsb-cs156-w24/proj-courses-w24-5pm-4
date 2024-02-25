faa0a12c7fccc544b4e111e7a96125eabe9025da
Cory Zhao <coryzhao80796@gmail.com>
2024-02-24 17:13:36 -0800

    Committing branch

--- frontend/src/main/pages/PersonalSchedules/PersonalSchedulesEditPage.js
+++ frontend/src/main/pages/PersonalSchedules/PersonalSchedulesEditPage.js
@@ -1,12 +1,72 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from "react-router-dom";
+import PersonalScheduleForm from "main/components/PersonalSchedules/PersonalScheduleForm";
+import { Navigate } from 'react-router-dom'
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function PersonalSchedulesEditPage() {
-  return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>Personal Schedules</h1>
-        <p>This is where the edit page will go</p>
-      </div>
-    </BasicLayout>
-  );
+export default function PersonalSchedulesEditPage({storybook=false}) {
+    let { id } = useParams();
+
+    const { data: personalSchedule, _error, _status } =
+        useBackend(
+            // Stryker disable next-line all : don't test internal caching of React Query
+            [`/api/personalschedules?id=${id}`],
+            {  // Stryker disable next-line all : GET is the default, so mutating this to "" doesn't introduce a bug
+                method: "GET",
+                url: `/api/personalschedules`,
+                params: {
+                    id
+                }
+            }
+        );
+
+    const objectToAxiosPutParams = (personalSchedule) => ({
+        url: "/api/personalschedules",
+        method: "PUT",
+        params: {
+            id: personalSchedule.id,
+        },
+        data: {
+            user: personalSchedule.user,
+            name: personalSchedule.name,
+            description: personalSchedule.description,
+            quarter: personalSchedule.quarter
+        }
+    });
+
+    const onSuccess = (personalSchedule) => {
+      toast(
+        `New personalSchedule Created - id: ${personalSchedule.id} name: ${personalSchedule.name}`,
+      );
+    };
+
+    const mutation = useBackendMutation(
+        objectToAxiosPutParams,
+        { onSuccess },
+        // Stryker disable next-line all : hard to set up test for caching
+        [`/api/personalschedules?id=${id}`]
+    );
+
+    const { isSuccess } = mutation
+
+    const onSubmit = async (data) => {
+        mutation.mutate(data);
+    }
+
+    if (isSuccess && !storybook) {
+        return <Navigate to="/personalschedules/list" />
+    }
+
+    return (
+        <BasicLayout>
+            <div className="pt-2">
+                <h1>Edit Personal Schedule</h1>
+                {
+                    personalSchedule && <PersonalScheduleForm submitAction={onSubmit} buttonLabel={"Update"} initialPersonalSchedule={personalSchedule} />
+                }
+            </div>
+        </BasicLayout>
+    )
+
 }
