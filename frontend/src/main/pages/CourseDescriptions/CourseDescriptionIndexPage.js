4982d6b203c2d8bc45546e53f78e3eefb5d25f88
Megumi Ondo <megumiondo@169-231-149-188.wireless.ucsb.edu>
2024-02-29 17:00:30 -0800

    mo - making fixes for delete

--- frontend/src/main/pages/CourseDescriptions/CourseDescriptionIndexPage.js
+++ frontend/src/main/pages/CourseDescriptions/CourseDescriptionIndexPage.js
@@ -1,43 +1,40 @@
-import { useState } from "react";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import BasicCourseSearchForm from "main/components/BasicCourseSearch/BasicCourseSearchForm";
-import BasicCourseTable from "main/components/Courses/BasicCourseTable";
-import { useBackendMutation } from "main/utils/useBackend";
+import _BasicCourseTable from "main/components/Courses/BasicCourseTable";
+import { useParams } from "react-router-dom";
+import { useBackend, _useBackendMutation } from "main/utils/useBackend";
+import CourseDetailsTable from "main/components/CourseDetails/CourseDetailsTable";
+import { yyyyqToQyy } from "main/utils/quarterUtilities";
 
-export default function CourseDescriptionIndexPage() {
+export default function CourseDetailsIndexPage() {
   // Stryker disable next-line all : Can't test state because hook is internal
-  const [courseJSON, setCourseJSON] = useState([]);
-
-  const objectToAxiosParams = (query) => ({
-    url: "/api/public/basicsearch",
-    params: {
-      qtr: query.quarter,
-      dept: query.subject,
-      level: query.level,
+  let { qtr, enrollCode } = useParams();
+  const {
+    data: moreDetails,
+    _error,
+    _status,
+  } = useBackend(
+    // Stryker disable all : hard to test for query caching
+    [`/api/sections/sectionsearch?qtr=${qtr}&enrollCode=${enrollCode}`],
+    {
+      method: "GET",
+      url: `/api/sections/sectionsearch`,
+      params: {
+        qtr,
+        enrollCode,
+      },
     },
-  });
-
-  const onSuccess = (courses) => {
-    setCourseJSON(courses.classes);
-  };
-
-  const mutation = useBackendMutation(
-    objectToAxiosParams,
-    { onSuccess },
-    // Stryker disable next-line all : hard to set up test for caching
-    [],
   );
-
-  async function fetchBasicCourseJSON(_event, query) {
-    mutation.mutate(query);
-  }
 
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h5>Welcome to the UCSB Courses Description Search!</h5>
-        <BasicCourseSearchForm fetchJSON={fetchBasicCourseJSON} />
-        <BasicCourseTable courses={courseJSON} />
+        {moreDetails && moreDetails.courseId && (
+          <h5>
+            Course Details for {moreDetails.courseId} {yyyyqToQyy(qtr)}
+          </h5>
+        )}
+
+        {moreDetails && <CourseDetailsTable details={[moreDetails]} />}
       </div>
     </BasicLayout>
   );
