5d569a96a87fabe7e7c26dbf57303b0e0c5976cc
samanthwest <samanthawest@ucsb.edu>
2024-03-05 18:13:22 -0800

    sw - fixing my messed up merge from main into this branch

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -153,12 +153,6 @@ export default function AppNavbar({
                   >
                     Manage Jobs
                   </NavDropdown.Item>
-                  <NavDropdown.Item
-                    href="/developer"
-                    data-testid="appnavbar-admin-developer-info"
-                  >
-                    Developer Info
-                  </NavDropdown.Item>
                 </NavDropdown>
               )}
             </Nav>
