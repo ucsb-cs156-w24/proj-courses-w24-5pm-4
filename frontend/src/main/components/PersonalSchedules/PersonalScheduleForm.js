062d49a52db2099fb76c0b583b649df9820a2acc
Cory Zhao <coryzhao80796@gmail.com>
2024-03-06 13:24:03 -0800

    testing

--- frontend/src/main/components/PersonalSchedules/PersonalScheduleForm.js
+++ frontend/src/main/components/PersonalSchedules/PersonalScheduleForm.js
@@ -8,107 +8,107 @@ import { quarterRange } from "main/utils/quarterUtilities";
 import { useSystemInfo } from "main/utils/systemInfo";
 
 function PersonalScheduleForm({
- initialPersonalSchedule,
- submitAction,
- buttonLabel = "Create",
+  initialPersonalSchedule,
+  submitAction,
+  buttonLabel = "Create",
 }) {
- const { data: systemInfo } = useSystemInfo();
- // Stryker disable OptionalChaining
- const startQtr = systemInfo?.startQtrYYYYQ || "20211";
- const endQtr = systemInfo?.endQtrYYYYQ || "20244";
- // Stryker enable OptionalChaining
- const quarters = quarterRange(startQtr, endQtr);
- // Stryker disable all
- const {
-   register,
-   formState: { errors },
-   handleSubmit,
- } = useForm({ defaultValues: initialPersonalSchedule || {} });
- // Stryker restore all
+  const { data: systemInfo } = useSystemInfo();
+  // Stryker disable OptionalChaining
+  const startQtr = systemInfo?.startQtrYYYYQ || "20211";
+  const endQtr = systemInfo?.endQtrYYYYQ || "20244";
+  // Stryker enable OptionalChaining
+  const quarters = quarterRange(startQtr, endQtr);
+  // Stryker disable all
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ defaultValues: initialPersonalSchedule || {} });
+  // Stryker restore all
 
+  const navigate = useNavigate();
+  const [quarter, setQuarter] = useState({
+    yyyyq: initialPersonalSchedule?.quarter,
+    qyy:
+      initialPersonalSchedule?.name.substring(0, 1) +
+      initialPersonalSchedule?.name.substring(
+        initialPersonalSchedule?.name.length - 2,
+        initialPersonalSchedule?.name.length,
+      ),
+  });
+  return (
+    <Form onSubmit={handleSubmit(submitAction)}>
+      {initialPersonalSchedule && (
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="id">Id</Form.Label>
+          <Form.Control
+            data-testid="PersonalScheduleForm-id"
+            id="id"
+            type="text"
+            {...register("id")}
+            value={initialPersonalSchedule.id}
+            disabled
+          />
+        </Form.Group>
+      )}
 
- const navigate = useNavigate();
- const [quarter, setQuarter] = useState(
-   {
-     yyyyq: initialPersonalSchedule?.quarter,
-     qyy: initialPersonalSchedule?.name.substring(0,1) + initialPersonalSchedule?.name.substring(initialPersonalSchedule?.name.length - 2,initialPersonalSchedule?.name.length)
-   }
- );
- return (
-   <Form onSubmit={handleSubmit(submitAction)}>
-     {initialPersonalSchedule && (
-       <Form.Group className="mb-3">
-         <Form.Label htmlFor="id">Id</Form.Label>
-         <Form.Control
-           data-testid="PersonalScheduleForm-id"
-           id="id"
-           type="text"
-           {...register("id")}
-           value={initialPersonalSchedule.id}
-           disabled
-         />
-       </Form.Group>
-     )}
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="name">Name</Form.Label>
+        <Form.Control
+          data-testid="PersonalScheduleForm-name"
+          id="name"
+          type="text"
+          isInvalid={Boolean(errors.name)}
+          {...register("name", {
+            required: "Name is required.",
+            maxLength: {
+              value: 15,
+              message: "Max length 15 characters",
+            },
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.name?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
 
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="description">Description</Form.Label>
+        <Form.Control
+          data-testid="PersonalScheduleForm-description"
+          id="description"
+          type="text"
+          isInvalid={Boolean(errors.description)}
+          {...register("description", {
+            required: "Description is required.",
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.description?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Form.Group className="mb-3" data-testid="PersonalScheduleForm-quarter">
+        <SingleQuarterDropdown
+          quarter={quarter}
+          setQuarter={setQuarter}
+          controlId={"PersonalScheduleForm-quarter"}
+          label={"Quarter"}
+          quarters={quarters}
+        />
+      </Form.Group>
 
-     <Form.Group className="mb-3">
-       <Form.Label htmlFor="name">Name</Form.Label>
-       <Form.Control
-         data-testid="PersonalScheduleForm-name"
-         id="name"
-         type="text"
-         isInvalid={Boolean(errors.name)}
-         {...register("name", {
-           required: "Name is required.",
-           maxLength: {
-             value: 15,
-             message: "Max length 15 characters",
-           },
-         })}
-       />
-       <Form.Control.Feedback type="invalid">
-         {errors.name?.message}
-       </Form.Control.Feedback>
-     </Form.Group>
-
-
-     <Form.Group className="mb-3">
-       <Form.Label htmlFor="description">Description</Form.Label>
-       <Form.Control
-         data-testid="PersonalScheduleForm-description"
-         id="description"
-         type="text"
-         isInvalid={Boolean(errors.description)}
-         {...register("description", {
-           required: "Description is required.",
-         })}
-       />
-       <Form.Control.Feedback type="invalid">
-         {errors.description?.message}
-       </Form.Control.Feedback>
-     </Form.Group>
-     <Form.Group className="mb-3" data-testid="PersonalScheduleForm-quarter">
-       <SingleQuarterDropdown
-         quarter={quarter}
-         setQuarter={setQuarter}
-         controlId={"PersonalScheduleForm-quarter"}
-         label={"Quarter"}
-         quarters={quarters}
-       />
-     </Form.Group>
-
-     <Button type="submit" data-testid="PersonalScheduleForm-submit">
-       {buttonLabel}
-     </Button>
-     <Button
-       variant="Secondary"
-       onClick={() => navigate(-1)}
-       data-testid="PersonalScheduleForm-cancel"
-     >
-       Cancel
-     </Button>
-   </Form>
- );
+      <Button type="submit" data-testid="PersonalScheduleForm-submit">
+        {buttonLabel}
+      </Button>
+      <Button
+        variant="Secondary"
+        onClick={() => navigate(-1)}
+        data-testid="PersonalScheduleForm-cancel"
+      >
+        Cancel
+      </Button>
+    </Form>
+  );
 }
 
 export default PersonalScheduleForm;
--- frontend/src/main/components/Quarters/SingleQuarterDropdown.js
+++ frontend/src/main/components/Quarters/SingleQuarterDropdown.js
@@ -14,54 +14,53 @@ import { Form } from "react-bootstrap";
 //  { yyyyq :"20222", qyy: "S22"}]
 
 function SingleQuarterDropdown({
- quarter,
- quarters,
- setQuarter,
- controlId,
- onChange = null,
- label = "Quarter",
+  quarter,
+  quarters,
+  setQuarter,
+  controlId,
+  onChange = null,
+  label = "Quarter",
 }) {
- const localSearchQuarter = localStorage.getItem(controlId);
- if (!localSearchQuarter) {
-   localStorage.setItem(controlId, quarters[0].yyyyq);
- }
- const [quarterState, setQuarterState] = useState(
-   // Stryker disable next-line all : not sure how to test/mock local storage
-   //localSearchQuarter || quarters[0].yyyyq,
-   quarter.yyyyq || localSearchQuarter
- );
+  const localSearchQuarter = localStorage.getItem(controlId);
+  if (!localSearchQuarter) {
+    localStorage.setItem(controlId, quarters[0].yyyyq);
+  }
 
+  const [quarterState, setQuarterState] = useState(
+    // Stryker disable next-line all : not sure how to test/mock local storage
+    //localSearchQuarter || quarters[0].yyyyq,
+    quarter.yyyyq || localSearchQuarter || quarters[0].yyyyq,
+  );
 
- const handleQuarterOnChange = (event) => {
-   const selectedQuarter = event.target.value;
-   localStorage.setItem(controlId, selectedQuarter);
-   setQuarterState(selectedQuarter);
-   setQuarter(selectedQuarter);
-   if (onChange != null) {
-     onChange(event);
-   }
- };
+  const handleQuarterOnChange = (event) => {
+    const selectedQuarter = event.target.value;
+    localStorage.setItem(controlId, selectedQuarter);
+    setQuarterState(selectedQuarter);
+    setQuarter(selectedQuarter);
+    if (onChange != null) {
+      onChange(event);
+    }
+  };
 
- return (
-   <Form.Group controlId={controlId}>
-     <Form.Label>{label}</Form.Label>
-     <Form.Control
-       as="select"
-       value={quarterState}
-       onChange={handleQuarterOnChange}
-     >
-       {quarters.map(function (object, i) {
-         const key = `${controlId}-option-${i}`;
-         return (
-           <option key={key} data-testid={key} value={object.yyyyq}>
-             {object.qyy}
-           </option>
-         );
-       })}
-     </Form.Control>
-   </Form.Group>
- );
+  return (
+    <Form.Group controlId={controlId}>
+      <Form.Label>{label}</Form.Label>
+      <Form.Control
+        as="select"
+        value={quarterState}
+        onChange={handleQuarterOnChange}
+      >
+        {quarters.map(function (object, i) {
+          const key = `${controlId}-option-${i}`;
+          return (
+            <option key={key} data-testid={key} value={object.yyyyq}>
+              {object.qyy}
+            </option>
+          );
+        })}
+      </Form.Control>
+    </Form.Group>
+  );
 }
-
 
 export default SingleQuarterDropdown;
