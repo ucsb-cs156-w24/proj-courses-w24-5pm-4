bcec9d7e70da94f5fd7658e78f5ca0f80b71c2cf
Rui Tong <torry_tong@outlook.com>
2024-03-07 17:15:34 -0800

    solve bugs

--- frontend/src/main/components/Courses/CourseForm.js
+++ frontend/src/main/components/Courses/CourseForm.js
@@ -37,6 +37,11 @@ function CourseForm({ initialCourse, submitAction, buttonLabel = "Create" }) {
   // Stryker restore all
 
   useEffect(() => {
+
+    if (localSchedule && schedules && schedules.length > 0) {
+      schedules.find((sch) => sch === localSchedule);
+    }
+
     if (schedules && schedules.length > 0 && !localSchedule) {
       setSchedule(schedules[0].id);
       // Stryker disable all : not sure how to test/mock local storage
