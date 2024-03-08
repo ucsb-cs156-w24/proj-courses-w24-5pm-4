0290988a468440f575470399f7672946ede351c2
Rui Tong <torry_tong@outlook.com>
2024-03-07 18:03:39 -0800

    run prettier

--- frontend/src/main/components/Courses/CourseForm.js
+++ frontend/src/main/components/Courses/CourseForm.js
@@ -37,12 +37,13 @@ function CourseForm({ initialCourse, submitAction, buttonLabel = "Create" }) {
   // Stryker restore all
 
   useEffect(() => {
+    // Stryker disable all : not sure how to test/mock local storage
     if (localSchedule && schedules && schedules.length > 0) {
       schedules.find((sch) => sch === localSchedule);
     }
-    
-    if (schedules && schedules.length > 0 && !localSchedule) {
+    // Stryker restore all
 
+    if (schedules && schedules.length > 0 && !localSchedule) {
       setSchedule(schedules[0].id);
       // Stryker disable all : not sure how to test/mock local storage
       localStorage.setItem("CourseForm-psId", schedules[0].id);
